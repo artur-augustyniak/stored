6bd5b3d6bf5569016f6b631dfc0bbda921964497
Artur Augustyniak <artur@aaugustyniak.pl>
2016-08-23 15:34:37 +0200

    main cleanup

--- src/stored.c
+++ src/stored.c
@@ -2,8 +2,6 @@
 #include <config.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <pthread.h>
 #include "util/logger.h"
 #include "util/demonizer.h"
 #include "mtab_check_trigger.h"
